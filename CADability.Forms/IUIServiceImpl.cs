5613c39e2550b45c6a09bf94572f780b5fa2c2a2
Davide Bazzi <davidebazzi@pcam.com>
2020-11-04 15:21:03 +0100

    Implemented IUIService.ApplicationIdle

--- CADability.Forms/IUIServiceImpl.cs
+++ CADability.Forms/IUIServiceImpl.cs
@@ -209,12 +209,12 @@ namespace CADability.Forms
         {
             add
             {
-                throw new NotImplementedException();
+                Application.Idle += value;
             }
 
             remove
             {
-                throw new NotImplementedException();
+                Application.Idle -= value;
             }
         }
     }
