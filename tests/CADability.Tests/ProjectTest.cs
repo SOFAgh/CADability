95f79e8e2aa66b81bbe4cd9cd4201e4e7cd10bd6
Jürgen Steinblock <js@octoflex.de>
2022-10-10 16:39:26 +0200

    added mstest
    * added new mstest project
    * added basic unit test for importing a dxf file
    * small changes for building and testing the project with `dotnet` cli.

---
+++ tests/CADability.Tests/ProjectTest.cs
@@ -0,0 +1,36 @@
+﻿using CADability.Forms;
+
+namespace CADability.Tests
+{
+    [TestClass]
+    public class ProjectTest
+    {
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DeploymentItem(@"Files/Dxf/square_100x100.dxf", nameof(import_dxf_square_succeds))]
+        [DeploymentItem(@"Files/Dxf/square_100x100.bmp", nameof(import_dxf_square_succeds))]
+        public void import_dxf_square_succeds()
+        {
+            var file = Path.Combine(this.TestContext.DeploymentDirectory, this.TestContext.TestName, "square_100x100.dxf");
+            Assert.IsTrue(File.Exists(file));
+            var bmpFile = Path.Combine(this.TestContext.DeploymentDirectory, this.TestContext.TestName, "square_100x100.bmp");
+
+            var project = Project.ReadFromFile(file, "dxf");
+            Assert.IsNotNull(project);
+            var model = project.GetActiveModel();
+            Assert.IsNotNull(model);
+            var obj = Assert.That.Single(model.AllObjects);
+            var polyline = Assert.That.IsInstanceOfType<GeoObject.Polyline>(obj);
+            Assert.AreEqual(400, polyline.Length);
+
+
+            using (var expected = (Bitmap)Image.FromFile(bmpFile))
+            using (var actual = PaintToOpenGL.PaintToBitmap(model.AllObjects, GeoVector.ZAxis, 100, 100))
+            {
+                Assert.That.BitmapsAreEqual(expected, actual);
+            }
+        }
+    }
+}
