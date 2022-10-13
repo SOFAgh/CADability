82e7d9a09b5bf33a259c9fd4090219c77b374c93
Jürgen Steinblock <js@octoflex.de>
2022-10-13 16:48:05 +0200

    added failing test for issue #104

--- tests/CADability.Tests/ProjectTest.cs
+++ tests/CADability.Tests/ProjectTest.cs
@@ -1,4 +1,6 @@
 ﻿using CADability.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
 
 namespace CADability.Tests
 {
@@ -32,5 +34,47 @@ namespace CADability.Tests
                 Assert.That.BitmapsAreEqual(expected, actual);
             }
         }
+
+        [TestMethod]
+        [DeploymentItem(@"Files/Stp/issue104.stp", nameof(import_step_file_should_always_returns_a_solid))]
+        public void import_step_file_should_always_returns_a_solid()
+        {
+
+            // this unit test demonstrates that importing a step file sometimes
+            // returns a Shell instead of a Solid
+            var file = Path.Combine(this.TestContext.DeploymentDirectory, this.TestContext.TestName, "issue104.stp");
+            Assert.IsTrue(File.Exists(file));
+
+            var dict = new Dictionary<Type, int>();
+
+            // read the same file 100 times
+            // and add or update type count in dict
+            for (int i = 0; i < 100; i++)
+            {
+
+
+                var project = Project.ReadFromFile(file, "stp");
+                Assert.IsNotNull(project);
+                var model = project.GetActiveModel();
+                Assert.IsNotNull(model);
+
+                var obj = Assert.That.Single(model.AllObjects);
+                var type = obj.GetType();
+                if (!dict.ContainsKey(type))
+                {
+                    dict.Add(type, 1);
+                }
+                else
+                {
+                    dict[type]++;
+                }
+            }
+
+            Assert.AreEqual(1, dict.Count, "Outcome:" + String.Join(", ", dict.Select(x => $"{x.Key.Name} * {x.Value}")));
+            Assert.That.IsInstanceOfType<GeoObject.Solid>(dict.ElementAt(0).Key);
+
+
+        }
+
     }
 }
