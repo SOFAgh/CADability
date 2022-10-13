a9e5afec703c5cad16896b07de2bc9f1a63c844c
Jürgen Steinblock <js@octoflex.de>
2022-10-13 13:37:08 +0200

    fix for issue 102
    * added failing test
    * removed explicit OutwardOriented implementation

--- tests/CADability.Tests/ProjectTest.cs
+++ tests/CADability.Tests/ProjectTest.cs
@@ -1,4 +1,5 @@
 ﻿using CADability.Forms;
+using System.IO.Compression;
 
 namespace CADability.Tests
 {
@@ -32,5 +33,47 @@ namespace CADability.Tests
                 Assert.That.BitmapsAreEqual(expected, actual);
             }
         }
+
+        [TestMethod]
+        [DeploymentItem(@"Files/Step/issue101.stp", nameof(import_step_issue101_succeds))]
+        [DeploymentItem(@"Files/Step/issue101.png", nameof(import_step_issue101_succeds))]
+        public void import_step_issue101_succeds()
+        {
+            // cylinder.OutwardOriented throws an NotImplementedException
+            // because there is a concret implementation
+            //   public bool OutwardOriented => toCylinder.Determinant > 0;
+            // and an explicit interface implementation
+            //   bool ICylinder.OutwardOriented => throw new NotImplementedException();
+
+            var file = Path.Combine(this.TestContext.DeploymentDirectory, this.TestContext.TestName, "issue101.stp");
+            Assert.IsTrue(File.Exists(file));
+            var bmpFile = Path.Combine(this.TestContext.DeploymentDirectory, this.TestContext.TestName, "issue101.png");
+            Assert.IsTrue(File.Exists(bmpFile));
+
+            var project = Project.ReadFromFile(file, "stp");
+            Assert.IsNotNull(project);
+            var model = project.GetActiveModel();
+            Assert.IsNotNull(model);
+            Assert.AreEqual(1, model.AllObjects.Count);
+
+            var solid = Assert.That.IsInstanceOfType<GeoObject.Solid>(model.AllObjects[0]);
+            var shell = solid.Shells[0];
+            // get a cylinder (order faces and cylinders to always get the same entity)
+            var cylinder = shell
+                .Faces
+                .OrderBy(x => x.Area)
+                .Select(x => x.Surface)
+                .OfType<GeoObject.ICylinder>()
+                .OrderBy(x => x.Radius)
+                .FirstOrDefault();
+            Assert.IsTrue(cylinder.OutwardOriented);
+
+            using (var expected = (Bitmap)Image.FromFile(bmpFile))
+            using (var actual = PaintToOpenGL.PaintToBitmap(model.AllObjects, GeoVector.NullVector, 200, 200))
+            {
+                Assert.That.BitmapsAreEqual(expected, actual);
+            }
+        }
+
     }
 }
